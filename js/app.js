9550bc70c78e14cf96471501dd9b50f2d7bf1ab6
Matthew Holder <holdermatthew5@gmail.com>
2020-08-14 14:54:36 -0700

    added form

--- js/app.js
+++ js/app.js
@@ -11,7 +11,6 @@ var table = document.createElement('table');
 var thead = document.createElement('thead');
 var th = document.createElement('th');
 var tbody = document.createElement('tbody');
-// var tfoot = document.createElement('tfoot');
 main.append(article);
 h2.textContent = 'Cookies per hour';
 article.append(h2);
@@ -24,30 +23,35 @@ for (var i = 0; i <= hoursPerDay.length - 1; i++) {
   thead.append(th);
 }
 table.append(tbody);
-// tfoot.textContent = 'sum of hours';
-// table.append(tfoot);
 
+var hourlySumArr = [];
 // Creation of object
-
 function ObjectCreation(min, max, ave, city) {
   this.minCust = min;
   this.maxCust = max;
   this.aveOrder = ave;
   this.name = city;
   this.hourNums = [];
-  this.hourSum = [];
+  this.cookiesPerHour();
+  this.fillTable();
+  hourlySumArr.push(this);
 }
 
+// random number generator
 ObjectCreation.prototype.custPerHour = function (maximum) {
   return Math.floor(Math.random() * Math.floor(maximum));
 };
-ObjectCreation.prototype.cookiesPerHour = function() {
+
+// creates hourly sales
+ObjectCreation.prototype.cookiesPerHour = function () {
   for (var i = 0; i < hoursPerDay.length - 1; i++) {
     var cookiesThisHour = (this.aveOrder * this.custPerHour(9));
     this.hourNums.push(Math.round(cookiesThisHour));
   }
 };
-ObjectCreation.prototype.fillTable = function() {
+
+// fills table with daily sales
+ObjectCreation.prototype.fillTable = function () {
   var tr = document.createElement('tr');
   tbody.append(tr);
   var th = document.createElement('th');
@@ -65,34 +69,51 @@ ObjectCreation.prototype.fillTable = function() {
   td.textContent = dailyTotal;
   tr.append(td);
 };
-function hourlySum(){
+
+
+
+var Seattle = new ObjectCreation(23, 65, 6.3, 'Seattle');
+var Tokyo = new ObjectCreation(3, 24, 1.2, 'Tokyo');
+var Dubai = new ObjectCreation(11, 38, 3.7, 'Dubai');
+var Paris = new ObjectCreation(20, 38, 2.3, 'Paris');
+var Lima = new ObjectCreation(2, 16, 4.6, 'Lima');
+
+var tfoot = document.createElement('tfoot');
+table.append(tfoot);
+function hourlySum() {
   var tr = document.createElement('tr');
-  tbody.append(tr);
+  tfoot.append(tr);
   var th = document.createElement('th');
   th.textContent = 'Hourly sum';
   tr.append(th);
   var hourlySubTotal = [];
+  var hourlyTotal = 0;
   for (var j = 0; j < hoursPerDay.length; j++) {
-    var hourlyTotal = Seattle.hourNums[j] + Tokyo.hourNums[j] + Dubai.hourNums[j] + Paris.hourNums[j] + Lima.hourNums[j];
+    for (var i = 0; i < hourlySumArr.length; i++) {
+      hourlyTotal += hourlySumArr[i].hourNums[j];
+    }
     hourlySubTotal.push(hourlyTotal);
     var td = document.createElement('td');
     td.textContent = hourlySubTotal[j];
     tr.append(td);
   }
 }
-var Seattle = new ObjectCreation(23, 65, 6.3, 'Seattle');
-Seattle.cookiesPerHour();
-Seattle.fillTable();
-var Tokyo = new ObjectCreation(3, 24, 1.2, 'Tokyo');
-Tokyo.cookiesPerHour();
-Tokyo.fillTable();
-var Dubai = new ObjectCreation(11, 38, 3.7, 'Dubai');
-Dubai.cookiesPerHour();
-Dubai.fillTable();
-var Paris = new ObjectCreation(20, 38, 2.3, 'Paris');
-Paris.cookiesPerHour();
-Paris.fillTable();
-var Lima = new ObjectCreation(2, 16, 4.6, 'Lima');
-Lima.cookiesPerHour();
-Lima.fillTable();
+
+// replace footer
+
+// delete footer to add last row and call footer again
+
+// }
+// uses form to create object
+var listener = document.getElementById('submit');
+listener.addEventListener('submit', useForm);
+
+function useForm(event) {
+  event.preventDefault();
+  var nameInput = event.target.city.value;
+  var minInput = event.target.mincust.value;
+  var maxInput = event.target.maxcust.value;
+  var aveInput = event.target.aveorder.value;
+  new ObjectCreation(minInput, maxInput, aveInput, nameInput);
+}
 hourlySum();
