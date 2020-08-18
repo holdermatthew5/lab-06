51c83d51eab28bd9cb3f95eefe8abb25c9a5e900
Matthew Holder <holdermatthew5@gmail.com>
2020-08-17 20:14:52 -0700

    fixed hourly sum replacement

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 var hoursPerDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total'];
 
-// table creation
+// Creates table
 
 var main = document.getElementById('main');
 var article = document.createElement('article');
@@ -25,7 +25,7 @@ for (var i = 0; i <= hoursPerDay.length - 1; i++) {
 table.append(tbody);
 
 var hourlySumArr = [];
-// Creation of object
+// Prepares to create object
 function ObjectCreation(min, max, ave, city) {
   this.minCust = min;
   this.maxCust = max;
@@ -70,14 +70,14 @@ ObjectCreation.prototype.fillTable = function () {
   tr.append(td);
 };
 
-
-
+// Creates objects
 var Seattle = new ObjectCreation(23, 65, 6.3, 'Seattle');
 var Tokyo = new ObjectCreation(3, 24, 1.2, 'Tokyo');
 var Dubai = new ObjectCreation(11, 38, 3.7, 'Dubai');
 var Paris = new ObjectCreation(20, 38, 2.3, 'Paris');
 var Lima = new ObjectCreation(2, 16, 4.6, 'Lima');
 
+// creates hourly sum row in tfoot
 var tfoot = document.createElement('tfoot');
 table.append(tfoot);
 function hourlySum() {
@@ -94,42 +94,29 @@ function hourlySum() {
     }
     hourlySubTotal.push(hourlyTotal);
     var td = document.createElement('td');
+    td.setAttribute('id', 'update' + j);
     td.textContent = hourlySubTotal[j];
     tr.append(td);
   }
 }
 
-// replace footer
-function hourlySumReplacement() {
-  tfoot.remove();
-  var tfooter = document.createElement('tfoot');
-  table.append(tfooter);
-  var tr = document.createElement('tr');
-  tfooter.append(tr);
-  var th = document.createElement('th');
-  th.textContent = 'Hourly sum';
-  tr.append(th);
+// updates footer
+function updateTfoot() {
   var hourlySubTotal = [];
   var hourlyTotal = 0;
   for (var j = 0; j < hoursPerDay.length; j++) {
+    // var reference = 'td' + j;
     for (var i = 0; i < hourlySumArr.length; i++) {
       hourlyTotal += hourlySumArr[i].hourNums[j];
     }
     hourlySubTotal.push(hourlyTotal);
-    var td = document.createElement('td');
-    td.textContent = hourlySubTotal[j];
-    tr.append(td);
+    document.getElementById('update' + j).innerHTML = hourlySubTotal[j];
   }
-
 }
 
-// delete footer to add last row and call footer again
-
-// }
 // uses form to create object
 var listener = document.getElementById('submit');
 listener.addEventListener('submit', useForm);
-
 function useForm(event) {
   event.preventDefault();
   var nameInput = event.target.city.value;
@@ -137,6 +124,7 @@ function useForm(event) {
   var maxInput = event.target.maxcust.value;
   var aveInput = event.target.aveorder.value;
   new ObjectCreation(minInput, maxInput, aveInput, nameInput);
-  hourlySumReplacement();
+  updateTfoot();
+  // hourlySumReplacement();
 }
 hourlySum();
